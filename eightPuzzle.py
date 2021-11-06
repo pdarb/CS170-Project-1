148d754a09eb4cbe123481fca9a10abbb0fd9a9e
pdarb <pdarb001@ucr.edu>
2021-11-06 01:29:20 -0700

    Added direction move functions

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -1,86 +1,88 @@
 from copy import deepcopy
+from queue import Queue
+from enum import Enum
 import math
 
+class MoveDirection(Enum):
+    Up = (-1, 0)
+    Down = (1, 0)
+    Right = (0, 1)
+    Left = (0, -1)
+
+
 class Puzzle:
-    def __init__ (self, start, next = None):
+    def __init__ (self, start):
         self.startState = start
         self.gameState = start
 
-        self.goalState = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']]
+        self.goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
         self.g = 0
         self.h = 0
         self.f = 0
+        self.zero_index = []
 
-    def manhattan_dist_heuristic(matrix):
+
+    # Calculate the # of misplaced tiles of current state 
+    def misplaced_tile_heuristic(self): 
+        count = 0
+        for i in range(len(self.gameState)):
+            for j in range(len(self.gameState[i])):
+                if self.startState[i][j] != self.goalState[i][j] and self.gameState[i][j] != 0:
+                    count += 1
+        print(count)
+    
+
+    # Calculate manhattan distance of current state
+    def manhattan_dist_heuristic(self): 
+        thisdict = {
+            "1": [0,0],
+            "2": [0,1],
+            "3": [0,2],
+            "4": [1,0],
+            "5": [1,1],
+            "6": [1,2],
+            "7": [2,0],
+            "8": [2,1],
+            "0": [2,2]
+        }
+        
         dist = 0
-        for i in range(len(random)):
-            for j in range(len(random[i])):
-                if random[i][j] != goal[i][j] and random[i][j] != 0:
-                    goalPos = thisdict[str(random[i][j])]
-                    #print(goalPos)
+        for i in range(len(self.gameState)):
+            for j in range(len(self.gameState[i])):
+                if self.gameState[i][j] != self.goalState[i][j] and self.gameState[i][j] != 0:
+                    goalPos = thisdict[str(self.gameState[i][j])]
+                    print(goalPos)
                     dist += (abs(i - goalPos[0]) + abs(j - goalPos[1]))
         print(dist)
 
-    def misplaced_tile_heuristic(self, start, goal):
-        count = 0
-        for i in range(len(random)):
-            for j in range(len(random[i])):
-                if random[i][j] != goal[i][j]:
-                    count += 1
-        print(count)
-        
 
-    #def 
-    # def manhattan_algorithm(self, )
+    def find_zero_index(self):
+        for i in range(len(self.gameState)):
+            for j in range(len(self.gameState[i])):
+                if self.gameState[i][j] == 0:
+                    self.zero_index = [i, j]
+                    print(self.zero_index)
 
-    # def print_initial_state(self):
+
+    def direction_result(self, state, move):
+        # Add conditions to check edge cases
+        val = [sum(x) for x in zip(self.zero_index, move.value)]
+        print(val)
+
+        # a = self.gameState[self.zero_index[0]][self.zero_index[1]]
+        # b = self.gameState[val[0]][val[1]]
+
+        #a, b = b, a
+
+        self.gameState[self.zero_index[0]][self.zero_index[1]], self.gameState[val[0]][val[1]] = self.gameState[val[0]][val[1]], self.gameState[self.zero_index[0]][self.zero_index[1]]
+        print(self.gameState)
+
+
+    def print_initial_state(self):
+        print('Matrix: ', self.gameState)
 
 
     def solution_check(self):
         return self.gameState == self.goalState
 
 
-
-matrix = []
-
-input_string = input('Enter first row: ')
-user_list = input_string.split()
-
-for i in range(len(user_list)):
-    # convert each item to int type
-    user_list[i] = int(user_list[i])
-
-matrix.append(user_list)
-
-input_string = input('Enter second row: ')
-user_list = input_string.split()
-
-for i in range(len(user_list)):
-    # convert each item to int type
-    user_list[i] = int(user_list[i])
-
-matrix.append(user_list)
-
-input_string = input('Enter third row: ')
-user_list = input_string.split()
-
-for i in range(len(user_list)):
-    # convert each item to int type
-    user_list[i] = int(user_list[i])
-
-matrix.append(user_list)
-
-for i in matrix:
-    for j in i:
-        print(j, end=' ')
-    print(' ')
-
-for i in range(len(user_list)):
-    if user_list[i] == 0:
-        print(int(user_list[i]))
-    #else
-
-#algChoice = input('Choose (1)Uniform Cost ')
-
-
-print('Matrix: ', matrix)
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from eightPuzzle import Puzzle
+from eightPuzzle import MoveDirection
+
+
+# def uniform_cost_algorithm():
+
+
+
+# def misplaced_tile_algorithm():
+
+
+
+# def manhattan_distance_algorithm():
 
 
 
@@ -6,8 +18,71 @@ from eightPuzzle import Puzzle
 
 
 
+if __name__== '__main__':
 
-# if __main__=='main':
+
+    matrix = []
+
+    input_string = input('Enter first row: ')
+    user_list = input_string.split()
+
+    for i in range(len(user_list)):
+        # convert each item to int type
+        user_list[i] = int(user_list[i])
+
+    matrix.append(user_list)
+
+    input_string = input('Enter second row: ')
+    user_list = input_string.split()
+
+    for i in range(len(user_list)):
+        # convert each item to int type
+        user_list[i] = int(user_list[i])
+
+    matrix.append(user_list)
+
+    input_string = input('Enter third row: ')
+    user_list = input_string.split()
+
+    for i in range(len(user_list)):
+        # convert each item to int type
+        user_list[i] = int(user_list[i])
+
+    matrix.append(user_list)
+
+    for i in matrix:
+        for j in i:
+            print(j, end=' ')
+        print(' ')
+
+    for i in range(len(user_list)):
+        if user_list[i] == 0:
+            print(int(user_list[i]))
+    #else
+
+    algChoice = int(input('Choose: \n(1) Uniform Cost \n(2) Misplaced Tiles \n(3) Manhattan Distance\n'))
+
+    game = Puzzle(matrix)
+    #d = MoveDirection()
+
+    if algChoice == 2:
+        game.misplaced_tile_heuristic()
+
+    if algChoice == 3:
+        game.manhattan_dist_heuristic()
+
+    game.find_zero_index()
+
+    game.direction_result(matrix, MoveDirection.Up)    
+
+   # game.find_zero_index()
+
+    game.print_initial_state()
+
+    
+
+
 
     #depth: 3
-    game = Puzzle([['1', '2', '3'], ['0', '4', '6'], ['7', '5', '8']])
+    #game = Puzzle([['1', '2', '3'], ['0', '4', '6'], ['7', '5', '8']])
+
