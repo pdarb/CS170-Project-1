7807698223c07b3fd2dab3c88d7dea3df849e9b9
pdarb <pdarb001@ucr.edu>
2021-11-07 02:19:49 -0800

    Misplaced tiles and Manhattan distance algs.

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from queue import Queue
 from enum import Enum
+import numpy as np
 import math
 
 class MoveDirection(Enum):
@@ -17,7 +18,7 @@ class Puzzle:
 
         self.goalState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
         self.g = 0
-        self.h = 0
+        #self.h = 0
         self.f = 0
         self.zero_index = []
 
@@ -30,6 +31,7 @@ class Puzzle:
                 if self.startState[i][j] != self.goalState[i][j] and self.gameState[i][j] != 0:
                     count += 1
         print(count)
+        return count
     
 
     # Calculate manhattan distance of current state
@@ -54,6 +56,7 @@ class Puzzle:
                     print(goalPos)
                     dist += (abs(i - goalPos[0]) + abs(j - goalPos[1]))
         print(dist)
+        return dist
 
 
     def find_zero_index(self):
@@ -62,27 +65,52 @@ class Puzzle:
                 if self.gameState[i][j] == 0:
                     self.zero_index = [i, j]
                     print(self.zero_index)
+        return self.zero_index
+        
 
 
     def direction_result(self, state, move):
-        # Add conditions to check edge cases
-        val = [sum(x) for x in zip(self.zero_index, move.value)]
-        print(val)
+        
+        state = self.copy_matrix(state)
+
+        # Add conditions to check edge cases, check for valid moves
+        
+        self.find_zero_index() # Finds position of 0 before moving
+        #if (self.zero_index[0] < 0)
+
+        swapPos = [sum(x) for x in zip(self.zero_index, move.value)]
+
+        if swapPos[0] >= 0 and swapPos[0] <= 2 and swapPos[1] >=0 and swapPos[1] <=2:
+            temp = state[swapPos[0]][swapPos[1]]
+            state[self.zero_index[0]][self.zero_index[1]] = temp
+            state[swapPos[0]][swapPos[1]] = 0
+            return state
+        else:
+            return ''
+
 
         # a = self.gameState[self.zero_index[0]][self.zero_index[1]]
         # b = self.gameState[val[0]][val[1]]
 
         #a, b = b, a
+        # self.gameState[self.zero_index[0]][self.zero_index[1]], self.gameState[val[0]][val[1]] = self.gameState[val[0]][val[1]], self.gameState[self.zero_index[0]][self.zero_index[1]]
+        # return self.gameState
 
-        self.gameState[self.zero_index[0]][self.zero_index[1]], self.gameState[val[0]][val[1]] = self.gameState[val[0]][val[1]], self.gameState[self.zero_index[0]][self.zero_index[1]]
-        print(self.gameState)
 
+    def copy_matrix(self): #makes a copy of current matrix
+        return deepcopy(self.gameState)
+        
 
     def print_initial_state(self):
         print('Matrix: ', self.gameState)
 
 
-    def solution_check(self):
-        return self.gameState == self.goalState
+    def goal_test(self):
+        #print('Reached goal test')
+        isEqual = np.array_equal(self.gameState, self.goalState) 
+        #print('Is equal:', isEqual)
+        return isEqual
+
+        
 
 
--- main.py
+++ main.py
@@ -1,17 +1,239 @@
-from eightPuzzle import Puzzle
-from eightPuzzle import MoveDirection
+from eightPuzzle import Puzzle, MoveDirection
+from queue import Queue, PriorityQueue
 
 
-# def uniform_cost_algorithm():
+def uniform_cost_algorithm(game):
+
+    print('Entered Uniform cost alg')
+
+    depth = 0
+    q = Queue()
+    q.put(game)
+   
+    while not q.empty():
+
+        #depth += 1
+
+        startGame = q.get()  #pops first node and stores it in startGame var.
+
+        if startGame.goal_test(): #checks if curr_state = goal_state
+            print('Solved using Uniform Cost!')
+            return 
+
+        #checks all possible direction moves
+        try:
+            newGame = Puzzle(startGame)
+            newMove = newGame.direction_result(newGame.gameState, MoveDirection.Up)
+            q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            print('checking down')
+            newGame = Puzzle(startGame)
+            newMove = newGame.direction_result(newGame.gameState, MoveDirection.Down)
+            q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            print('checking left')
+            newGame = Puzzle(startGame)
+            newMove = newGame.direction_result(newGame.gameState, MoveDirection.Left)
+            q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            print('checking right')
+            newGame = Puzzle(startGame)
+            newMove = newGame.direction_result(newGame.gameState, MoveDirection.Right)
+            q.put(newMove)
+
+        except:
+            pass
+ 
+
+
+def misplaced_tile_algorithm(game):
+
+    depth = 0
+    q = PriorityQueue()
+    q.put(game)
+
+    while not q.empty():
+
+        depth += 1
+
+        startGame = q.get()  #pops first node and stores it in startGame var.
+
+        if startGame.goal_test(): #checks if curr_state = goal_state
+            print('Solved using Misplaced Tile!!')
+            return
+
+        #check all possible direction moves
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Up)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('up', newMove.startState)
+                newMove.h = newMove.misplaced_tile_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Down)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('down', newMove.startState)
+                newMove.h = newMove.misplaced_tile_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Left)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('left', newMove.startState)
+                newMove.h = newMove.misplaced_tile_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Right)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('right', newMove.startState)
+                newMove.h = newMove.misplaced_tile_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+        except:
+            pass
 
 
 
-# def misplaced_tile_algorithm():
+        bestMove=''
+        leastScore=100000000
+    
+        for move in q.queue:
+            if move.f < leastScore:
+                bestMove = move
+                leastScore = move.f
+        
+        print('BestMove =>>', bestMove.gameState)
+        q = PriorityQueue()
+        q.put(bestMove)
+
+    
 
 
 
-# def manhattan_distance_algorithm():
+def manhattan_distance_algorithm(game):
+    
+    depth = 0
+    q = PriorityQueue()
+    q.put(game)
+    print(q.qsize())
 
+    while not q.empty():
+
+        depth += 1
+
+        startGame = q.get()  #pops first node and stores it in startGame var.
+
+        if startGame.goal_test(): #checks if curr_state = goal_state
+            print('Solved using Manhattan Distance!!')
+            return
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Up)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('up', newMove.startState)
+                newMove.h = newMove.manhattan_dist_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Down)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('down', newMove.startState)
+                newMove.h = newMove.manhattan_dist_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+
+        except:
+            pass
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Left)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('left', newMove.startState)
+                newMove.h = newMove.manhattan_dist_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+        except:
+            pass
+
+        try:
+            newMove = startGame.direction_result(startGame.startState, MoveDirection.Right)
+            if newMove:
+                newMove = Puzzle(newMove)
+                print('right', newMove.startState)
+                newMove.h = newMove.manhattan_dist_heuristic()
+                newMove.f = depth + newMove.h
+                print('h', newMove.h)
+                print('f', newMove.f)
+                q.put(newMove)
+
+        except:
+            pass
+
+
+        bestMove=''
+        leastScore=100000000
+    
+        for move in q.queue:
+            if move.f < leastScore:
+                bestMove = move
+                leastScore = move.f
+        
+        print('BestMove =>>', bestMove.gameState)
+        q = PriorityQueue()
+        q.put(bestMove)
 
 
 
@@ -65,19 +287,27 @@ if __name__== '__main__':
     game = Puzzle(matrix)
     #d = MoveDirection()
 
+    if algChoice == 1:
+        uniform_cost_algorithm(game)
+
     if algChoice == 2:
-        game.misplaced_tile_heuristic()
+        misplaced_tile_algorithm(game)
 
     if algChoice == 3:
-        game.manhattan_dist_heuristic()
+        manhattan_distance_algorithm(game)
 
-    game.find_zero_index()
 
-    game.direction_result(matrix, MoveDirection.Up)    
 
-   # game.find_zero_index()
 
-    game.print_initial_state()
+
+    #game.find_zero_index()
+
+    #game.direction_result(matrix, MoveDirection.Up)    
+
+    #game.misplaced_tile_heuristic()
+    #game.manhattan_dist_heuristic()
+
+    # game.print_initial_state()
 
     
 
